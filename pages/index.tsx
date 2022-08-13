6a0cebca0857ce3d6d65447fcbc682d6a04c06bb
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-08-13 23:54:00 +0800

    Added Head Tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className="grid w-screen h-screen place-items-center">
-      <p className="text-4xl font-bold align-middle font-manrope">
-        Hello world!
-      </p>
-    </div>
+    <>
+      <Head>
+        <title> Home | Cotton Road</title>
+        <meta name="description" content="Home Page of Cotton Road" />
+      </Head>
+      <div className="grid w-screen h-screen place-items-center">
+        <p className="text-4xl font-bold align-middle font-manrope">
+          Welcome to Cotton Road!
+        </p>
+      </div>
+    </>
   );
 }
