c33a7bcb49e314ec2b5398f887e6748cb83e9ef1
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-08-14 20:27:19 +0800

    Done with landing Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,25 +32,32 @@ export default function Index() {
       </section>
       <section
         id="lm"
-        className="grid w-2/3 h-screen grid-cols-1 mx-auto sm:grid-cols-2 sm:w-4/5 place-items-center"
+        className="relative grid w-2/3 h-screen grid-cols-1 mx-auto sm:grid-cols-2 sm:w-full place-items-center"
       >
-        <div>
-          <p className="text-2xl text-left">
-            A place where secondary schools can buy merchendise from one another
-          </p>
+        <div className="grid w-full h-1/5 place-items-center">
+          <div>
+            <p className="text-3xl text-center align-middle sm:text-left">
+              A place where students can
+            </p>
+            <p className="text-3xl text-center align-middle sm:text-left">
+              buy merchendise from one another
+            </p>
+          </div>
         </div>
         <div className="w-full h-full">
-          <Deal className="w-full h-full text-indigo-500" />
+          <Deal className="block w-full h-full text-indigo-500" />
+          <div className="absolute right-0 block w-2/4 overflow-hidden bg-purple-100 rounded-l-full bottom-20 -z-10 h-2/5"></div>
         </div>
       </section>
-      <section className="grid w-2/3 h-screen grid-cols-1 mx-auto sm:grid-cols-2 sm:w-4/5 place-items-center">
+      <section className="relative grid w-2/3 h-screen grid-cols-1 mx-auto sm:grid-cols-2 sm:w-full place-items-center">
         <div className="w-full h-full">
-          <Stripe className="w-full h-full text-indigo-500" />
+          <Stripe className="block w-full h-full text-indigo-500" />
         </div>
         <div>
           <p className="text-3xl text-center align-middle sm:text-right">
             Payments are made secure using Stripe
           </p>
+          <div className="absolute left-0 block w-2/4 overflow-hidden bg-purple-100 rounded-r-full bottom-20 -z-10 h-2/5"></div>
         </div>
       </section>
       <section className="grid w-3/4 h-screen mx-auto place-items-center">
