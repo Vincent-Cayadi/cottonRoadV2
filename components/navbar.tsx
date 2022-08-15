1b5954133828ea7e7a733cecb1d1fb4953f22553
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-08-15 21:09:53 +0800

    navbar component

---
+++ components/navbar.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Link from "next/link";
+import {
+  AiOutlineFire,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+} from "react-icons/ai";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="sticky top-0 z-50 hidden sm:block">
+        <nav className="flex justify-between w-full py-4 mx-auto bg-white">
+          <p className="m-auto text-left align-middle ">Cotton Road</p>
+          <input
+            type="text"
+            placeholder="Search.."
+            className="w-2/3 px-4 py-2 duration-300 border border-gray-300 rounded-md hover:border-gray-900 "
+          />
+
+          <div className="m-auto">
+            <Link href="/latest">
+              <div className="inline-block mx-4 cursor-pointer">
+                <AiOutlineFire size="1.8rem" className="mx-auto" />
+                <p className="text-sm align-middle ">Latest</p>
+              </div>
+            </Link>
+            <Link href="/cart">
+              <div className="inline-block mx-4 cursor-pointer">
+                <AiOutlineShoppingCart size="1.8rem" className="mx-auto" />
+                <p className="text-sm align-middle">Cart</p>
+              </div>
+            </Link>
+            <div className="inline-block mx-4 cursor-pointer">
+              <AiOutlineUser size="1.8rem" className="mx-auto" />
+              <p className="text-sm align-middle">Login</p>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}
