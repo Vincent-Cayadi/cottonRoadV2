5a92c2d02178730625658e85bff3523153ad0fb9
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-10-26 14:22:55 +0800

    Managed to pull data through prisma

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Navbar from "../components/navbar";
 import Card from "../components/card";
+
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-export default function Home() {
+
+export default function Home({ data }) {
   return (
     <>
       <Head>
@@ -14,10 +16,23 @@ export default function Home() {
       <main>
         <section className="mx-auto sm:w-4/5 bg-grey-600">
           <h1 className="w-full my-2 text-2xl text-left">New Arrivals</h1>
+          <ul>
+            {data.map((item) => (
+              <li key="item.id">{item.name}</li>
+            ))}
+          </ul>
           <h1 className="w-full my-2 text-2xl text-left">Schools</h1>
           <h1 className="w-full my-2 text-2xl text-left">Projects</h1>
         </section>
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const data = await prisma.products.findMany();
+
+  return {
+    props: { data },
+  };
 }
